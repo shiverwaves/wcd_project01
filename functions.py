1b358c422a720e3a7defa0d388a15eabf767af61
shiverwaves <118776591+shiverwaves@users.noreply.github.com>
2023-09-30 01:34:36 -0400

    updated user_data

--- functions.py
+++ functions.py
@@ -188,6 +188,39 @@ def create_ec2(client, config):
             MaxCount=1,
             SecurityGroupIds=[config['sg_id']],
             SubnetId=config['subnet_id'],
+            UserData="""#!/bin/bash
+
+# update
+apt update -y
+
+# install python 3.10
+apt install software-properties-common -y
+add-apt-repository ppa:deadsnakes/ppa -y
+apt install python3.10 -y
+python3.10 --version
+
+# install node 18
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+apt install -y nodejs
+node --version
+
+# install java 11
+install java 11.0
+apt-get install openjdk-11-jdk -y
+
+# install docker engine
+apt-get update
+apt-get install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+docker --version""",
             TagSpecifications=tag_specs
         )
         time.sleep(1)
